61bed2c96fbdc17570a5dff8886c2d507ab57485
OlegShinka <volkonxxl@gmail.com>
2023-08-18 21:57:06 +0300

    timer Fil

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -15,22 +15,32 @@ refs.timerEl.style.justifyContent = 'space-around';
 
 refs.inputEl.addEventListener('input', onInputTime);
 refs.startEl.addEventListener('click', onStartTimer);
-
 refs.startEl.setAttribute('disabled', true);
-const date = new Date();
+
 let resConvert;
+let selectData;
+let idTimer;
+let remainTime;
 
 function onInputTime(evt) {
   evt.preventDefault();
 }
-
 function onStartTimer() {
-  setInterval(() => {
-    addLeadingZero(resConvert);
+  idTimer = setInterval(() => {
+    const realTime = new Date().getTime(); //*** */
+    remainTime = selectData - realTime;
+    resConvert = convertMs(remainTime);
+    // console.log('selectedData', selectData);
+
+    if (remainTime > 0) {
+      addLeadingZero();
+    } else {
+      clearInterval(idTimer);
+    }
   }, 1000);
 }
 
-function addLeadingZero(val) {
+function addLeadingZero() {
   refs.daysEl.textContent = resConvert.days.toString().padStart(2, '0');
   refs.hoursEl.textContent = resConvert.hours.toString().padStart(2, '0');
   refs.minutesEl.textContent = resConvert.minutes.toString().padStart(2, '0');
@@ -38,43 +48,31 @@ function addLeadingZero(val) {
 }
 
 const fp = flatpickr(refs.inputEl, {
-  altInput: true,
-  altFormat: 'F j, Y',
-  dateFormat: 'Y-m-d',
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+
   onClose(selectedDates) {
-    const timerId = setInterval(() => {
-      const realTime = Date.now();
-      const selectedTime = selectedDates[0].getTime();
-      const remainTime = Math.floor(selectedTime - realTime);
+    const realTime = new Date();
 
-      resConvert = convertMs(remainTime);
-
-      if (remainTime <= 0) {
-        clearInterval(timerId);
-        return;
-      }
-    }, 1000);
-
-    if (selectedDates[0] < date) {
+    if (selectedDates[0] < realTime) {
       refs.startEl.setAttribute('disabled', true);
       alert('Please choose a date in the future');
+      return;
     } else {
       refs.startEl.removeAttribute('disabled', true);
     }
+    //тут у нас отримані мілісекунди з input при виборі дати перед. в глобальну змінну
+    selectData = selectedDates[0].getTime();
   },
 });
-
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
   // Remaining days
   const days = Math.floor(ms / day);
   // Remaining hours
@@ -83,6 +81,5 @@ function convertMs(ms) {
   const minutes = Math.floor(((ms % day) % hour) / minute);
   // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
   return { days, hours, minutes, seconds };
 }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,29 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const fulfil = Math.random() > 0.4;
+    if (fulfil) {
+      resolve('Succesful');
+    }
+    reject('Errorr');
+  }, 2000);
+});
+
+promise
+  .then(result => {
+    console.log('Winner JS');
+    console.log(`${Succesful}`);
+  })
+  .catch(result => {
+    console.log('NOT Winner JS');
+    // console.log(`${Errorr}`);
+  });
+console.log(promise);
