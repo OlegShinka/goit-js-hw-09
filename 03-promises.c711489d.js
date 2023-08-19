0c5b2b20e96e4b1b36b3b6098a236d475db36b07
OlegShinka <128846423+OlegShinka@users.noreply.github.com>
2023-08-19 14:49:38 +0000

    Deploying to gh-pages from @ OlegShinka/goit-js-hw-09@f079e144a8de59e367f0e699c14eb20d430efa8d 🚀

--- 03-promises.c711489d.js
+++
@@ -1,2 +0,0 @@
-!function(){var n=new Promise((function(n,o){setTimeout((function(){Math.random()>.4&&n("Succesful"),o("Errorr")}),2e3)}));n.then((function(n){console.log("Winner JS"),console.log("".concat(Succesful))})).catch((function(n){console.log("NOT Winner JS")})),console.log(n)}();
-//# sourceMappingURL=03-promises.c711489d.js.map
